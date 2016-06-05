04910476a5e60c09765d7fa765c5520e38640ffa
gangwang <perfect590@126.com>
2016-06-05 21:35:16 +0800

    Add View to presenter.

--- app/src/main/java/com/christmas/strawberryweibo/presenter/imp/HomeFragmentPresenterImp.java
+++ app/src/main/java/com/christmas/strawberryweibo/presenter/imp/HomeFragmentPresenterImp.java
@@ -1,6 +1,16 @@
 package com.christmas.strawberryweibo.presenter.imp;
 
+import com.christmas.strawberryweibo.model.StatusListModel;
+import com.christmas.strawberryweibo.model.imp.StatusListModelImp;
 import com.christmas.strawberryweibo.presenter.HomeFragmentPresenter;
+import com.christmas.strawberryweibo.view.HomeFragmentView;
 
 public class HomeFragmentPresenterImp implements HomeFragmentPresenter {
+  private HomeFragmentView homeFragmentView;
+  private StatusListModel statusListModel;
+
+  public HomeFragmentPresenterImp(HomeFragmentView homeFragmentView) {
+    this.homeFragmentView = homeFragmentView;
+    statusListModel = new StatusListModelImp();
+  }
 }

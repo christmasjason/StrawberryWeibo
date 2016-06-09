18c554918b6bf36853f3d176f4453f610fb470a7
gangwang <perfect590@126.com>
2016-06-09 15:46:01 +0800

    Add log utils.

--- app/src/main/java/com/christmas/strawberryweibo/infrastructure/StrawberryApplication.java
+++ app/src/main/java/com/christmas/strawberryweibo/infrastructure/StrawberryApplication.java
@@ -2,9 +2,13 @@ package com.christmas.strawberryweibo.infrastructure;
 
 import android.app.Application;
 
+import com.christmas.strawberryweibo.util.Logger;
+
 public class StrawberryApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+
+    Logger.getLogger().i(getClass().getSimpleName() + " started.");
   }
 }
